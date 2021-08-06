702c8584094c320fccbef20dedca16ce66bb5f05
Wkwk <vladislav.igumnow@yandex.ru>
2021-08-06 16:41:14 +0300

    first commit

---
+++ Project/App/views.py
@@ -0,0 +1,12 @@
+from .models import Post
+from django.views import generic
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+
+class DetailView(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
